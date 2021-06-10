13bb1fad9dc51fa72e31d7c51ce0f13bb0c9b4b6
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-06-10 14:33:25 -0700

    added more options

--- Priyaprat.cpp
+++ Priyaprat.cpp
@@ -21,7 +21,9 @@ void PrintWelcomeMessage() {
 }
 
 void PrintOptions() {
-    std::cout << "1)Start Game\n";
+    std::cout << "1) Start Game\n";
+    std::cout << "2) Options\n";
+    std::cout << "3) Quit\n";
 }
 
 int main()
