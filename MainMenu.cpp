014b3d21ad5ebd72cae227f09caf9f33cc867623
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-06-14 19:23:00 -0700

    finished solution

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -8,13 +8,13 @@ public:
 	std::string mainOptions[3] = { "Start Game","Options","Quit" };
 	int optionNum{};
 
-	MainMenu() : Menu(mainOptions, sizeof(mainOptions) / sizeof(mainOptions[0]) ) {
-		
+	MainMenu() {
+		optionNum = sizeof(mainOptions) / sizeof(mainOptions[0]);
 	}
 
 	void PrintWelcomeMessage() {
 		std::cout << "Welcome to Priyaprat, survive if you can!\n";
-		PrintOptions();
+		PrintOptions(mainOptions, optionNum);
 		GetEnteredOption();
 	}
 };
