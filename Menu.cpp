27f54f70c196089ead5b9f1052bd0b0250fc700c
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-06-15 19:31:19 -0700

    changed getenteredoptions function to remove extra lines and add initial prompt

--- Menu.cpp
+++ Menu.cpp
@@ -2,8 +2,6 @@
 #include <string>
 
 class Menu{
-protected:
-
 
 public:
 
@@ -11,17 +9,17 @@ public:
         for (int i = 0; i < len; i++) {
             std::cout << i << ") " + options[i] + "\n";
         }
-
-        std::cout << std::endl;
     }
 
     int GetEnteredOption() {
         std::string rawNumber = "";
         int number{};
+
+        std::cout << "Enter: ";
         std::getline(std::cin, rawNumber);
 
         while (rawNumber.length() != 1 || !isdigit(rawNumber[0])) {
-            std::cout << "\nPlease enter a valid option: ";
+            std::cout << "Please enter a valid option: ";
             std::getline(std::cin, rawNumber);
         }
 
