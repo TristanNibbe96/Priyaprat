60d2fe260dff2be2242c99a99cc8df14a4a114af
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-06-21 22:45:14 -0700

    figured out error, came from conflicting access modifieries in implementation vs interface

--- Menu.h
+++ Menu.h
@@ -6,11 +6,13 @@ class Menu {
 
 public:
     virtual void Start() = 0;
-    virtual void OpeningMessage() = 0;
-    virtual void InputLoop() = 0;
 
     void PrintOptions(std::string options[], int len);
     int calcNumberOfOptions(std::string options[]);
     int GetEnteredOption();
     Menu() {};
+
+private:
+    virtual void OpeningMessage() = 0;
+    virtual void InputLoop() = 0;
 };
