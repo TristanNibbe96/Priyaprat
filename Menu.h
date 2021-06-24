4a97b39bbc8a1248539b84f0d1806fac175e382b
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-06-24 16:06:10 -0700

    filled out .h file for menuoption class

--- Menu.h
+++ Menu.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "MenuOption.cpp"
+#include "MenuOption.h"
 
 class Menu {
 
--- MenuOption.h
+++ MenuOption.h
@@ -3,3 +3,13 @@
 
 enum ScreenType { Main, Game, Options, };
 
+class MenuOption {
+
+public:
+	MenuOption(std::string s, ScreenType screen);
+	void SetText(std::string s);
+	void SetScreen(ScreenType screen);
+	std::string GetText();
+	ScreenType GetScreen();
+};
+
